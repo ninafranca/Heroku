5fd2d3fe7173c98b4f0ffaa64402413d9f1f97db
Nina Spinelli <ninafspin@gmail.com>
2022-02-22 01:49:33 -0300

    using Heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { __dirname } from "./utils.js"
 
 //VARIABLES
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 const server = app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
@@ -15,7 +15,4 @@ app.use(express.static(__dirname + "/public"))
 //APP.GET
 app.get("/info", (req, res) => {
     res.send({message: `Port ${PORT}`})
-})
-app.get("/", (req, res) => {
-    res.sendFile("index.html", {root: __dirname + "/public"})
 })
